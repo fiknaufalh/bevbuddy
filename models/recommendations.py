39b324a929d5c7bc718a483ad8d405dfc875d255
Fikri Naufal Hamdi <fikrinaufalh9@gmail.com>
2023-11-07 04:43:04 +0700

    feat: create recommendation request schema models

--- models/recommendations.py
+++ models/recommendations.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Literal
 
 class Recommendation(BaseModel):
     id_rec: int
@@ -13,5 +14,23 @@ class Recommendation(BaseModel):
                 "id_menu": 1,
                 "id_weather": 1,
                 "mood": "Happy"
+            }
+        }
+
+class RecommendationReq(BaseModel):
+    gender: Literal ["Male", "Female"]
+    age: int
+    weight: float
+    height: float
+    activity: Literal ["sedentary", "lightly_active", "moderately_active", "very_active", "extra_active"]
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "gender": "Male",
+                "age": 20,
+                "weight": 63.0,
+                "height": 171.0,
+                "activity": "lightly_active"
             }
         }
