268004351fe9ced5e631e0bdeb515a2e12246127
Fikri Naufal Hamdi <fikrinaufalh9@gmail.com>
2023-11-07 01:01:26 +0700

    refactor: change database name env config

--- utils/database_manager.py
+++ utils/database_manager.py
@@ -12,8 +12,8 @@ db_user = config['DB_USER']
 db_password = config['DB_PASSWORD']
 db_host = config['DB_HOST']
 db_port = config['DB_PORT']
-db_database = config['DB_DATABASE']
+db_name = config['DB_NAME']
 db_sslmode = config['DB_SSLMODE']
 
-db_engine = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_database}?sslmode={db_sslmode}"
+db_engine = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?sslmode={db_sslmode}"
 dbInstance = DatabaseManager(db_engine)
