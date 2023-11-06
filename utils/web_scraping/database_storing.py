e797fc969c9f3babfbfa0217377434a0a5aea21f
Fikri Naufal Hamdi <fikrinaufalh9@gmail.com>
2023-11-07 02:27:28 +0700

    feat: create web scraping program and data

---
+++ utils/web_scraping/database_storing.py
@@ -0,0 +1,54 @@
+import json
+import mysql.connector
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+config = dotenv_values(".env")
+
+config = {
+    'host': config['DB_HOST'],
+    'user': config['DB_USER'],
+    'password': config['DB_PASSWORD'],
+    'database': config['DB_NAME'],
+    'ssl_ca': config['DB_SSLMODE']  
+}
+
+def insert_data_to_mysql(json_file_path, config, table_name):
+    try:
+        with open(json_file_path, 'r') as json_file:
+            data = json.load(json_file)
+
+        connection = mysql.connector.connect(**config)
+        if connection.is_connected():
+            cursor = connection.cursor()
+
+            count = 1
+            for item in data:
+                try:
+                    columns = ', '.join(item.keys())
+                    placeholders = ', '.join(['%s'] * len(item))
+                    query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+                    values = tuple(item.values())
+
+                    print(f"{count}. {query} {values}\n")
+                    count += 1
+
+                    cursor.execute(query, values)
+                except:
+                    continue
+
+            connection.commit()
+            print("Data is already saved in: ", table_name)
+
+    except mysql.connector.Error as error:
+        print("Error:", error)
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("Connection closed.")
+
+if __name__ == "__main__":
+    insert_data_to_mysql(r"utils/web_scraping/data/menu.json", config, "menu")
+    insert_data_to_mysql(r"utils/web_scraping/data/nutrition.json", config, "menu_nutrition")
---
+++ utils/web_scraping/web_scraping.py
@@ -0,0 +1,119 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchWindowException, WebDriverException
+from webdriver_manager.chrome import ChromeDriverManager
+import traceback
+import json
+import re
+
+def main():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    wait = WebDriverWait(driver, 10)
+
+    URL = "https://www.starbucks.com/menu"
+
+    menu_table = []
+    nutrition_table = []
+
+    driver.maximize_window()
+    driver.get(URL)
+
+    wait.until(EC.presence_of_element_located((By.ID, "drinks")))
+    drinks = driver.find_element(By.ID, "drinks")
+    categories_link = [(category.text, category.get_attribute("href")) 
+                        for category in drinks.find_elements(By.TAG_NAME, "a")]
+
+    menus = []
+    exclude_category = ["coffee-travelers"]
+    for category_link in categories_link:
+        driver.get(category_link[1])
+        wait.until(EC.presence_of_element_located((By.TAG_NAME, "section")))
+        subcategories = driver.find_elements(By.TAG_NAME, "section")
+        menus_web = [subcategory.find_elements(By.TAG_NAME, "a") 
+                    for subcategory in subcategories if subcategory.get_attribute("id") not in exclude_category]
+
+        for menu in menus_web:
+            for item in menu:
+                menus.append((item.get_attribute("data-e2e"), item.get_attribute("href")))
+
+    for menu in menus:
+        driver.get(f"{menu[1]}/nutrition")
+
+        try:
+            wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "h1 + div")))
+            nutrition_dict = extract_nutrition(driver, menu[1])
+            menu_dict = extract_menu(driver, menu[1])
+
+            nutrition_table.append(nutrition_dict)
+            menu_table.append(menu_dict)
+        except:
+            traceback.print_exc()
+            continue
+
+    export_to_json(menu_table, r"/utils/web_scraping/data/menu.json")
+    export_to_json(nutrition_table, r"/utils/web_scraping/data/nutrition.json")
+
+
+def export_to_json(data, filename):
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+
+def extract_product_id(url):
+    pattern = r'/product/(\d+)/'
+    match = re.search(pattern, url)
+    
+    if match:
+        product_id = match.group(1)
+        return int(product_id)
+    else:
+        return None
+
+def extract_nutrition(driver, menu):
+    nutrition_dict = {
+        "id_menu": "",
+        "calories": "",
+        "protein": "",
+        "fats": "",
+        "carbs": "",
+        "sugar": "",
+    }
+
+    nutrition_dict["id_menu"] = extract_product_id(menu)
+
+    nutrition_list = driver.find_elements(By.CSS_SELECTOR, "div[data-e2e='nutritionTable'] > div")
+
+    nutrition_dict["calories"] = float(nutrition_list[0].find_element(
+        By.CSS_SELECTOR, "span > span + span").text.split()[0])
+    nutrition_dict["protein"] = float(nutrition_list[5]. find_element(
+        By.CSS_SELECTOR, "span > span + span").text.split()[0])
+    nutrition_dict["fats"] = float(nutrition_list[1].find_element(
+        By.CSS_SELECTOR, "span > span + span").text.split()[0])
+    nutrition_dict["carbs"] = float(nutrition_list[4].find_element(
+        By.CSS_SELECTOR, "span > span + span").text.split()[0])
+    nutrition_dict["sugar"] = float(nutrition_list[4].find_element(
+        By.CSS_SELECTOR, "div + div + div > div > span > span + span").text.split()[0])
+
+    return nutrition_dict
+
+def extract_menu(driver, menu):
+    menu_dict = {
+        "id": "",
+        "name": "",
+        "desc": "",
+        "category": "",
+        "url_img": ""
+    }
+
+    menu_dict["id"] = extract_product_id(menu)
+    menu_dict["name"] = driver.find_element(By.CSS_SELECTOR, "h1").text
+    menu_dict["desc"] = driver.find_element(By.CSS_SELECTOR, "p[data-e2e='productDescription']").text.strip('"')
+    menu_dict["category"] = driver.find_elements(By.CSS_SELECTOR, "a[class='text-noUnderline']")[1].text
+    menu_dict["url_img"] = driver.find_element(By.TAG_NAME, "img").get_attribute("src")
+
+    return menu_dict
+
+if __name__ == "__main__":
+    main()
