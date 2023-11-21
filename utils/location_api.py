b2a3e313b7e70cdc4ab4468d0a7fb78f0b0102fa
Fikri Naufal Hamdi <18221096@std.stei.itb.ac.id>
2023-11-22 02:32:27 +0700

    refactor: split location API and weather API utils

---
+++ utils/location_api.py
@@ -0,0 +1,17 @@
+import requests
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+config = dotenv_values(".env")
+
+API_KEY = config['API_KEY']
+
+def get_location(ip_address):
+    url = f"https://api.ipgeolocation.io/ipgeo?apiKey={API_KEY}&ip={ip_address}"
+    response = requests.get(url).json()
+
+    lat = response['latitude']
+    long = response['longitude']
+    rec_time = response["time_zone"]["current_time"]
+
+    return lat, long, rec_time
--- utils/weather_api.py
+++ utils/weather_api.py
@@ -1,28 +1,11 @@
 import requests
-from dotenv import load_dotenv, dotenv_values
 
-load_dotenv()
-config = dotenv_values(".env")
+def get_weather(lat, long): 
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&current=temperature_2m,precipitation,wind_speed_10m"
+    response = requests.get(url).json()
 
-API_KEY = config['API_KEY']
+    temperature = response['current']['temperature_2m']
+    precipitation = response['current']['precipitation']
+    wind_speed = response['current']['wind_speed_10m']
 
-def get_weather(ip_address): 
-    # default ip address is for handling local testing
-    if ip_address == "127.0.0.1":
-        ip_address = "8.8.8.8"
-
-    url_location = f"https://api.ipgeolocation.io/ipgeo?apiKey={API_KEY}&ip={ip_address}"
-    response_location = requests.get(url_location).json()
-
-    lat = response_location['latitude']
-    long = response_location['longitude']
-
-    url_weather = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&current=temperature_2m,precipitation,wind_speed_10m"
-    response_weather = requests.get(url_weather).json()
-
-    rec_time = response_location["time_zone"]["current_time"]
-    temperature = response_weather['current']['temperature_2m']
-    precipitation = response_weather['current']['precipitation']
-    wind_speed = response_weather['current']['wind_speed_10m']
-
-    return rec_time, temperature, precipitation, wind_speed
+    return temperature, precipitation, wind_speed
